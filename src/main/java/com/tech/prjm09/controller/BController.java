65c6b5153e41c9bafa855bc28816fd46de373897
Cha-Hyunju <hj4530hj271@naver.com>
2025-06-26 12:40:02 +0900

    cj2s1

--- src/main/java/com/tech/prjm09/controller/BController.java
+++ src/main/java/com/tech/prjm09/controller/BController.java
@@ -52,18 +52,25 @@ public class BController {
 	
 	@RequestMapping("/write")
 	private String write(HttpServletRequest request, Model model) {
-		model.addAttribute("request",request);
-		command=new BWriteCommand();
-		command.execute(model);
+//		model.addAttribute("request",request);
+//		command=new BWriteCommand();
+//		command.execute(model);
+		String bname=request.getParameter("bname");
+		String btitle=request.getParameter("btitle");
+		String bcontent=request.getParameter("bcontent");
+		iDao.write(bname, btitle, bcontent);
 		
 		return "redirect:list";
 	}
 	
 	@RequestMapping("/content_view")
 	private String content_view(HttpServletRequest request,Model model) {
-		model.addAttribute("request", request);
-		command=new BContentCommand();
-		command.execute(model);
+//		model.addAttribute("request", request);
+//		command=new BContentCommand();
+//		command.execute(model);
+		String bid=request.getParameter("bid");
+		BDto dto=iDao.contentView(bid);
+		model.addAttribute("content_view",dto);
 		
 		return "content_view";
 	}
